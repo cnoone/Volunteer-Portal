2b6c67231927686098c2c3039bb406ebdf42d39e
Cooper Noone <cn@counterpartysystems.com>
2011-08-10 12:13:44 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VolunteerPortal::Application.config.secret_token = '8fbe813c1385924b57c201db05610b0caf2d6e5dc47dd7bc70246d4b6f7a2a6288766bc71cfc5ee5d87b1ac481ab60d764306493207c470d6e4bfb48969efb10'
